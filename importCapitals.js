a9901f347fb85b33466563a87cfbf976cfa44803
leylaEngineer <leylamakri@gmail.com>
2024-11-09 20:40:23 +0100

    Fix __dirname for CSV import

--- importCapitals.js
+++ importCapitals.js
@@ -2,7 +2,10 @@ import fs from 'fs';
 import path from 'path';
 import pg from 'pg';
 import fastcsv from 'fast-csv';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const db = new pg.Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
